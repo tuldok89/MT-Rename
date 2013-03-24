5cde82114d62148a8f790d9e958a3a20f92b2723
Denver Ivan B. Gazo <tuldok89@gmail.com>
2013-03-24 19:16:45 +0800

    Initial Commit

---
+++ MT_Rename/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MT_Rename
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = System.IO.Directory.GetFiles(System.IO.Directory.GetCurrentDirectory());
+            foreach (var file in files)
+            {
+                // get the position
+                var pos = System.IO.Path.GetFileName(file).IndexOf('-');
+                var dirName = System.IO.Path.GetDirectoryName(file);
+                var origName = System.IO.Path.GetFileName(file);
+                // get string minus the number and dash
+                var newName = System.IO.Path.GetFileName(file).Substring(pos + 1);
+                var newPathName = System.IO.Path.Combine(dirName, newName);
+
+                Console.Write(origName);
+                Console.Write(" -> ");
+                Console.WriteLine(newName);
+
+                // Rename
+                System.IO.File.Move(file, newPathName);
+            }
+            Console.WriteLine("\nPress Any Key to Continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+This is a small commandline utility to automatically remove file numbers in files downloaded from Mangatraders.com
+
+To use this, simply run the program in a directory that contains the downloaded files.
